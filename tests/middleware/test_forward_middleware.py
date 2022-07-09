0e579d5aa767508fd7894bfe2e759eafdca95761
Tom <tom.neuschulten@uni-wuppertal.de>
2022-07-09 14:55:45 +0200

    Rudimentary tests

---
+++ tests/middleware/test_forward_middleware.py
@@ -0,0 +1,79 @@
+"""Unittests for the authentication middlware"""
+from typing import Any, Generator
+
+import pytest
+import requests
+from fastapi.testclient import TestClient
+from requests.models import Response
+
+from nlp_land_prediction_endpoint import __version__
+from nlp_land_prediction_endpoint.app import app
+from nlp_land_prediction_endpoint.utils import settings as Settings
+from nlp_land_prediction_endpoint.utils.remote_storage_controller import (
+    remote_storage_controller,
+)
+
+
+@pytest.fixture
+def client() -> Generator:
+    """Get the test client for tests and reuse it.
+
+    Yields:
+        Generator: Yields the test client as input argument for each test.
+    """
+    with TestClient(app) as tc:
+        yield tc
+
+    Settings.get_settings().NODE_TYPE = "SECONDARY"
+
+
+@pytest.fixture
+def endpoint() -> str:
+    """Get the login_endpoint for tests.
+
+    Returns:
+        str: The refresh_endpoint including current version.
+    """
+    return f"/api/v{__version__.split('.')[0]}/models/"
+
+
+@pytest.fixture
+def mock_forward_request(monkeypatch: Any) -> None:
+    """Simulates a request.get call by overwriting the function
+
+    Arguments:
+        monkeypatch: a monkeypatch object
+    """
+
+    def mock_post(*agrs, **kwargs):
+        # type: (*str, **int) -> Response
+        response = Response()
+        response.status_code = 200
+        response._content = b'{"message": "success"}'
+        return response
+
+    monkeypatch.setattr(requests, "get", mock_post)
+    monkeypatch.setattr(requests, "post", mock_post)
+
+
+def test_forward_middleware(client: TestClient, endpoint: str, mock_forward_request: Any) -> None:
+    Settings.get_settings().NODE_TYPE = "MAIN"
+    response = client.get(endpoint)
+    # assert response.status_code == 200
+    # response = client.post(endpoint, json={"modelType": "None", "modelSpecification" : {}})
+    assert response.status_code == 200
+    response = client.post(endpoint, json={"modelType": "lda"})
+    assert response.status_code == 200
+    response = client.get(endpoint + "123456789")
+    assert response.status_code == 404
+    response = client.get(endpoint + "1234")
+    assert response.status_code == 200
+    response = client.get("/forward")
+    assert response.status_code == 404
+
+    # TODO-TN Move this in seperate Test file
+    assert remote_storage_controller.find_model_in_remote_hosts("99") is None
+    assert not remote_storage_controller.get_remote_host("127.0.0.1") is None
+    assert remote_storage_controller.get_remote_host("0000") is None
+
+    Settings.get_settings().NODE_TYPE = "SECONDARY"
---
+++ tests/routes/test_hosts_route.py
@@ -0,0 +1,85 @@
+from typing import Generator
+
+import pytest
+from fastapi.testclient import TestClient
+
+from nlp_land_prediction_endpoint import __version__
+from nlp_land_prediction_endpoint.app import app
+from nlp_land_prediction_endpoint.models.model_hosts import RemoteHost
+from nlp_land_prediction_endpoint.routes.route_hosts import RemoteHostDeleteRequest
+
+
+@pytest.fixture
+def client() -> Generator:
+    """Get the test client for tests and reuse it.
+
+    Yields:
+        Generator: Yields the test client as input argument for each test.
+    """
+    with TestClient(app) as tc:
+        yield tc
+
+
+@pytest.fixture
+def endpoint() -> str:
+    """Get the endpoint for tests.
+
+    Returns:
+        str: The endpoint including current version.
+    """
+    return f"/api/v{__version__.split('.')[0]}/hosts/"
+
+
+@pytest.fixture
+def remote_host_creation_request() -> RemoteHost:
+    """Get a correct model creation request
+
+    Returns:
+        ModelCreationRequest: An correct modelcreation request
+    """
+    dummy_remote_host = {"ip": "localhost", "port": "666", "models": ["test"], "active_models": []}
+    return RemoteHost(**dummy_remote_host)
+
+
+@pytest.fixture
+def remote_host_deletion_request() -> RemoteHostDeleteRequest:
+    """Get a correct model deletion request
+
+    Returns:
+        ModelCreationRequest: An correct model deletion request
+    """
+    return RemoteHostDeleteRequest(ip="localhost")
+
+
+@pytest.fixture
+def failing_remote_host_deletion_request() -> RemoteHostDeleteRequest:
+    """Get a correct model deletion request
+
+    Returns:
+        ModelCreationRequest: An correct model deletion request
+    """
+    return RemoteHostDeleteRequest(ip="this.is.not.a.vaild.ip.....")
+
+
+def test_remote_hosts_add(
+    client: TestClient, endpoint: str, remote_host_creation_request: RemoteHost
+) -> None:
+    remote_host_list = client.get(endpoint).json()
+    response = client.post(endpoint, json=remote_host_creation_request.dict())
+    assert response.status_code == 200
+    assert remote_host_list != client.get(endpoint).json()
+
+
+def test_remote_hosts_delete(
+    client: TestClient, endpoint: str, remote_host_deletion_request: RemoteHostDeleteRequest
+) -> None:
+    response = client.delete(endpoint, json=remote_host_deletion_request.dict())
+    assert response.status_code == 200
+    assert response.json()["ip"] == remote_host_deletion_request.ip
+
+
+def test_remote_hosts_delete_fail(
+    client: TestClient, endpoint: str, failing_remote_host_deletion_request: RemoteHostDeleteRequest
+) -> None:
+    response = client.delete(endpoint, json=failing_remote_host_deletion_request.dict())
+    assert response.status_code == 404
